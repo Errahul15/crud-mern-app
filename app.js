c0c41aeb858b4e86bf9f45a5e2c22b95268a0396
Errahul15 <errahul532@gmail.com>
2022-06-28 14:37:28 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("./db/conn");
+const users = require("./models/userSchema");
+const cors = require("cors");
+const router = require("./routes/router");
+
+const port = process.env.PORT || 8003;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.json("server start")
+})
+
+app.use(router);
+
+if (process.env.NODE_ENV == 'production') {
+    app.use(express.static("client/build"));
+}
+
+app.listen(port, () => {
+    console.log(`server is start port number ${port}`);
+});
